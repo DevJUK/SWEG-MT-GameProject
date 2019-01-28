81868076b27600e82df239f343e53065c95f6366
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2019-01-28 23:05:28 +0000

    Cally
    Cally walk with animation, Sprint and jump commented out for now as wernt working with anim as intended but do work.
    
    Zomb still needs some ai work

--- UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Entity.cs
+++ UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Entity.cs
@@ -4,30 +4,99 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {    
-    public float MovementSpeed;   
+    public float WalkSpeed;
+    public float SprintSpeed;
 
     public float JumpStrength;
    
-    private Rigidbody Rigid;   
+    private Rigidbody Rigid;
+
+    Animator anim; //Assigns the animator
 
     private bool StartRotate = true;
+
+    private bool Moving;
 
     //set to one for no effect and only changed in traps;
     public float SpeedModifier = 1;       
     
     protected virtual void Start ()
     {
+        anim = GetComponent<Animator>(); //Calls animator
         Rigid = GetComponent<Rigidbody>();        
 	}	
 	
     public void Move(Vector3 MoveDir)
     {
-        Vector3 Vec;
-        Vec = new Vector3(MoveDir.x * MovementSpeed, Rigid.velocity.y, MoveDir.z * MovementSpeed);
-       
-        Rigid.velocity = Vec * SpeedModifier;
-    }
+        //if (Input.GetKey(Key:))
+        //{
+            if (Input.GetKey(KeyCode.W))
+            {
+                Moving = true;
 
+                anim.SetBool("IsWalking", true);
+
+                Vector3 Vec;
+                Vec = new Vector3(MoveDir.x * WalkSpeed, Rigid.velocity.y, MoveDir.z * WalkSpeed);
+
+                Rigid.velocity = Vec * SpeedModifier;
+            }
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    Moving = true;
+        //
+        //    anim.SetBool("IsWalkingL", true);
+        //
+        //    Vector3 Vec;
+        //    Vec = new Vector3(MoveDir.x * MovementSpeed, Rigid.velocity.y, MoveDir.z * MovementSpeed);
+        //
+        //    Rigid.velocity = Vec * SpeedModifier;
+        //}
+        //// else if (Input.GetKey(KeyCode.S))
+        //// {
+        ////     Moving = true;
+        ////
+        ////     anim.SetBool("IsWalking", true);
+        ////
+        ////     Vector3 Vec;
+        ////     Vec = new Vector3(MoveDir.x * MovementSpeed, Rigid.velocity.y, MoveDir.z * MovementSpeed);
+        ////
+        ////     Rigid.velocity = Vec * SpeedModifier;
+        //// }
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Moving = true;
+        //
+        //    anim.SetBool("IsWalkingR", true);
+        //
+        //    Vector3 Vec;
+        //    Vec = new Vector3(MoveDir.x * MovementSpeed, Rigid.velocity.y, MoveDir.z * MovementSpeed);
+        //
+        //    Rigid.velocity = Vec * SpeedModifier;
+        //}
+        //} //
+       // if (Input.GetKey(KeyCode.LeftShift))
+       // {
+       //     WalkSpeed = SprintSpeed;
+       //     
+       //
+       //    anim.SetBool("IsRunning", true);
+       //
+       //     //Vector3 Vec;
+       //     //Vec = new Vector3(MoveDir.x * SprintSpeed, Rigid.velocity.y, MoveDir.z * SprintSpeed);
+       //     //
+       //     //Rigid.velocity = Vec * SpeedModifier;
+       // }
+      //if(Input.GetKey(KeyCode.Space))
+      // {
+      //     anim.SetBool("IsJumping", true);
+      //     Jump();
+      // }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
 
     public void Jump()
     {
@@ -42,11 +111,12 @@ public class Entity : MonoBehaviour
     {
         RaycastHit HitInfo;
         return Physics.Raycast(transform.position, Vector3.down, out HitInfo, 1.2f);
+
     }
 
     public float GetSpeed()
     {
-        return MovementSpeed;
+        return WalkSpeed;
     }
 
     

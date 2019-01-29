35bc44c7c3ddfd588bdeeae7546a5cb367ad26c7
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2019-01-29 16:42:18 +0000

    Cally Update
    Cally now walks with W(Other buttons dont work unless w is pressed first.. will fix) Cally now sprints when shift is helf then goes back to walking when releaced Also animation for: Idle, Walking, Running all work correctly.

--- UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Entity.cs
+++ UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Entity.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
-{    
-    public float WalkSpeed;
-    public float SprintSpeed;
+{
+    public float MovementSpeed;
+    public float WalkSpeed = 2;
+    public float SprintSpeed = 4;
+    private float Stamina = 5.0f;
 
     public float JumpStrength;
    
@@ -22,6 +24,7 @@ public class Entity : MonoBehaviour
     
     protected virtual void Start ()
     {
+        MovementSpeed = WalkSpeed;
         anim = GetComponent<Animator>(); //Calls animator
         Rigid = GetComponent<Rigidbody>();        
 	}	
@@ -30,17 +33,34 @@ public class Entity : MonoBehaviour
     {
         //if (Input.GetKey(Key:))
         //{
-            if (Input.GetKey(KeyCode.W))
-            {
-                Moving = true;
+         if (Input.GetKey(KeyCode.W))
+         {
+              Moving = true;
 
-                anim.SetBool("IsWalking", true);
+              anim.SetBool("IsWalking", true);
 
-                Vector3 Vec;
-                Vec = new Vector3(MoveDir.x * WalkSpeed, Rigid.velocity.y, MoveDir.z * WalkSpeed);
+              Vector3 Vec;
+              Vec = new Vector3(MoveDir.x * MovementSpeed, Rigid.velocity.y, MoveDir.z * MovementSpeed);
 
-                Rigid.velocity = Vec * SpeedModifier;
-            }
+              Rigid.velocity = Vec * SpeedModifier;
+
+              if (Input.GetKey(KeyCode.LeftShift))
+              {
+                    MovementSpeed = SprintSpeed;
+
+                     anim.SetBool("IsRunning", true);
+
+                    //Vector3 Vec;
+                    //Vec = new Vector3(MoveDir.x * SprintSpeed, Rigid.velocity.y, MoveDir.z * SprintSpeed);
+                    //
+                    //Rigid.velocity = Vec * SpeedModifier;
+              }
+              else
+              {
+                    anim.SetBool("IsRunning", false);
+                    MovementSpeed = WalkSpeed;
+              }           
+        }
         //if (Input.GetKey(KeyCode.A))
         //{
         //    Moving = true;
@@ -75,26 +95,16 @@ public class Entity : MonoBehaviour
         //    Rigid.velocity = Vec * SpeedModifier;
         //}
         //} //
-       // if (Input.GetKey(KeyCode.LeftShift))
-       // {
-       //     WalkSpeed = SprintSpeed;
-       //     
-       //
-       //    anim.SetBool("IsRunning", true);
-       //
-       //     //Vector3 Vec;
-       //     //Vec = new Vector3(MoveDir.x * SprintSpeed, Rigid.velocity.y, MoveDir.z * SprintSpeed);
-       //     //
-       //     //Rigid.velocity = Vec * SpeedModifier;
-       // }
-      //if(Input.GetKey(KeyCode.Space))
-      // {
-      //     anim.SetBool("IsJumping", true);
-      //     Jump();
-      // }
+
+        //if(Input.GetKey(KeyCode.Space))
+        // {
+        //     anim.SetBool("IsJumping", true);
+        //     Jump();
+        // }
         else
         {
             anim.SetBool("IsWalking", false);
+            anim.SetBool("IsRunning", false);
         }
     }
 

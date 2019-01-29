42225ad30105cee813fea7c211574b21aaa0d550
Jonathan <jmc@devj.uk>
2019-01-29 11:40:39 +0000

    Inventory Work
    Added my inventory scripts from Mobile AE3 and begun work on making the inv in this game.

---
+++ UnityGameProject/Assets/Scripts/InventoryStuff/InvButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvButtonScript : MonoBehaviour
+{
+	public GameObject Inv;
+	private bool IsInvOpen;
+
+	private void Start()
+	{
+		if (Inv == null)
+		{
+			Debug.Log("Plase create an empty gameobject to store the hidden items, then attach it here!");
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("OpenInv"))
+		{
+			OpenInv();
+		}
+	}
+
+	public void OpenInv()
+	{
+		if (Inv != null) 
+		{
+			if (!IsInvOpen)
+			{
+				Time.timeScale = 0;
+				Inv.SetActive(true);
+				IsInvOpen = true;
+			}
+			else
+			{
+				Time.timeScale = 1;
+				Inv.SetActive(false);
+				IsInvOpen = false;
+			}
+		}
+	}
+}

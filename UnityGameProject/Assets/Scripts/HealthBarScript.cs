381e7abeb52e25851dec5c7725b40f488dae29f9
Jonathan <jmc@devj.uk>
2019-02-02 17:30:03 +0000

    Inventory Work #3 & Stats UI
    Added slots ordering & Stats UI for each stat, as well as radical sliders for the health and sanity.

---
+++ UnityGameProject/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+	public float Health = 10;
+	public Image HealthBar;
+
+
+	private void Update()
+	{
+		UpdateHealthBar(Health);
+	}
+
+
+	private void UpdateHealthBar(float Health)
+	{
+		float Amount = Health / 10;
+		HealthBar.fillAmount = Amount;
+	}
+}

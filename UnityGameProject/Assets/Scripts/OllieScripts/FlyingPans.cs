5cb39be4f2e4a21f6f41a8f3a405d1047d28bfc5
ThatBrummie <oliverneale44@gmail.com>
2019-02-13 16:46:41 +0000

    Pans
    Pans fly in random directions

---
+++ UnityGameProject/Assets/Scripts/OllieScripts/FlyingPans.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingPans : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Vector3 randomDirection;
+
+    public float MaxStrength = 5;
+    public float MinStrength = 1;
+
+    private float stregnth;
+
+    public void Awake()
+    {
+      
+    }
+
+    public float speed = 5.0f;
+    public void Start()
+    {
+        randomDirection = new Vector3(Random.value, Random.value, Random.value);
+        stregnth = Random.Range(MinStrength, MaxStrength);
+
+        if (!rb)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        transform.Rotate(randomDirection);
+        rb.AddForce(randomDirection * stregnth);
+
+
+    }
+
+}

b6adb77806ddebd2a65bbe0f5045cb4dd8d75655
Edward Goodhew <ejgoodhew@btinternet.com>
2019-02-14 03:55:41 +0000

    Headders and tooltips added to dialogue scripts

--- UnityGameProject/Assets/Scripts/Ed'sScripts/NPCDialogueScrpt.cs
+++ UnityGameProject/Assets/Scripts/Ed'sScripts/NPCDialogueScrpt.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class NPCDialogueScrpt : MonoBehaviour
 {
+    [Header("NPC ID")]
+    [Tooltip("Unique number granted to each npc (must be set manually)")]
+    public int NpcIDNumber;
+
+    [Header("NPC Dialogue Lines")]
+    [Tooltip("Put any dialogue you want the NPC to say in here, remember to use the in text triggers")]
+    public List<string> NPCDialogue;
+
     // Start is called before the first frame update
     void Start()
     {
--- UnityGameProject/Assets/Scripts/Ed'sScripts/NPCInteractionScrpt.cs
+++ UnityGameProject/Assets/Scripts/Ed'sScripts/NPCInteractionScrpt.cs
@@ -6,20 +6,32 @@ using UnityEngine.UI;
 public class NPCInteractionScrpt : MonoBehaviour
 {
     // Are you talking with a npc or not?
+    [Header("Interacting with the NPC")]
+    [Tooltip("Is the player interacting with the NPC")]
     public bool Interacting;
 
     // Dictates who starts talking first in conversation
+    [Header("Whos talking first")]
+    [Tooltip("If true the player is talking first. If false the NPC will talk first")]
     public bool PlayerTalksFirst;
 
     // The canvas that will hold all of the text boxes for dialogue
+    [Header("Canvas")]
+    [Tooltip("The canvas that will hold all of the text boxes for dialogue")]
     public GameObject InteractionCanvas;
 
     // Used to signify whos talking in conversation
+    [Header("Whos Talking")]
+    [Tooltip("Text box where the current talkers name goes")]
     public Text WhosTalking;
+    [Tooltip("Players name goes here")]
     public string PlayerName;
+    [Tooltip("NPC's name goes here")]
     public string NPCName;
 
-    // Textbox where dialogue will go
+    // Textbox where player dialogue will go
+    [Header("Player dialogue lines")]
+    [Tooltip("Put any dialogue you want the player to say in here, remember to use the in text triggers")]
     public Text DialogueBox;
 
     // Start is called before the first frame update

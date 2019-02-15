394dfbb6bf4abd28275351849fb666f775b8f377
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2019-02-15 16:54:53 +0000

    Few bits

---
+++ UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Event_Start.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_Start : MonoBehaviour
+{
+    public bool Event;
+    public GameObject Player;
+    public PlayerController MovementScript;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Event == true)
+        {
+
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("running");
+            MovementScript.enabled = false;
+            Event = true;
+
+        if(MovementScript == false)
+            {
+                anim.SetBool("IsWalking", false);
+                anim.SetBool("IsWalkingL", false);
+                anim.SetBool("IsWalkingR", false);
+                anim.SetBool("IsRunning", false);
+            }
+            
+        }
+    }
+}

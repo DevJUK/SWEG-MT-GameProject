e6e8dcc2d6dc7186d2a701cd25427cd367303b34
Jonathan <jmc@devj.uk>
2019-02-26 09:06:00 +0000

    Doors Work
    Doors are fully tested, they work with animations, there is a prefab in modular assets folder for these now.

--- UnityGameProject/Assets/Scripts/Ed'sScripts/DoorScript.cs
+++ UnityGameProject/Assets/Scripts/Ed'sScripts/DoorScript.cs
@@ -6,17 +6,6 @@ public class DoorScript : MonoBehaviour
 {
     public bool Locked;
     public bool DoorOpen;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void OpenDoor()
     {
@@ -29,23 +18,7 @@ public class DoorScript : MonoBehaviour
             if (DoorOpen)
             {
                 DoorOpen = false;
-                // Play closing animation
-
-				// -- Jonathan Addition Start --
-				if (GetComponentInChildren<Animator>())
-				{
-					foreach(Animator A in GetComponentsInChildren<Animator>())
-					{
-						A.SetTrigger("DoorOpened");
-					}
-				}
-				// -- Jonathan Addition End --
-
-                Debug.Log("Door is Open, closing door");
-            }
-            else
-            {
-                DoorOpen = true;
+				// Play closing animation
 
 				// -- Jonathan Addition Start --
 				if (GetComponentInChildren<Animator>())
@@ -57,10 +30,45 @@ public class DoorScript : MonoBehaviour
 				}
 				// -- Jonathan Addition End --
 
+				Debug.Log("Door is Open, closing door");
+            }
+            else
+            {
+                DoorOpen = true;
+
+				// -- Jonathan Addition Start --
+				if (GetComponentInChildren<Animator>())
+				{
+					foreach (Animator A in GetComponentsInChildren<Animator>())
+					{
+						A.SetTrigger("DoorOpened");
+					}
+				}
+				// -- Jonathan Addition End --
+
 				// Play Opening animation
 				Debug.Log("Door is closed, opening door");
             }
         }
 
     }
+
+
+	// -- Jonathan Addition Start --
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			OpenDoor();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			OpenDoor();
+		}
+	}
+	// -- Jonathan Addition End --
 }

9d452394600bbf0a3c3aa942dd3581b28947cb09
SoItsMaestro <samuelw1994@hotmail.com>
2019-02-28 18:55:34 +0000

    Added assets

---
+++ UnityGameProject/Assets/Textures/Materials/Scripts/InventoryStuff/RaycastItems.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastItems : MonoBehaviour
+{
+	public float Range;
+	public GameObject UI;
+	private bool UIOpen;
+
+	public PickupUIText PickupScript;
+
+    // Anything to do with NPC's in this script was done by Ed so talk to him
+
+    public NPCInteractionScrpt NPCInteractionScrpt; // the main controll script for npc dialogue interactions
+    public MirrorTipScript MirrorTipScript; // Script used to tip the mirror (who'd of guessed it)
+    public string NameofNPCHit; // Used to send the name of the NPC to the NPC interaction script
+
+	private void Start()
+	{
+		//PickupScript = GameObject.Find("PickupPopup").GetComponent<PickupUIText>();
+		//PickupScript.gameObject.SetActive(false);
+	}
+
+
+	private void FixedUpdate()
+	{
+		RaycastHit Hit;
+        
+
+
+		if (Physics.Raycast(transform.position, transform.forward, out Hit, Range)) // Check to see if raycast hits anything
+		{
+            // Check to see if hit object is a npc or item
+            if (Hit.transform.tag == "NPC") // if npc do this
+            {
+                // May need to change Name of model when using in actual game
+                NPCInteractionScrpt = GameObject.Find("Cally V1 Model@Idle").GetComponent<NPCInteractionScrpt>();
+
+                NameofNPCHit = Hit.transform.name; // getting the name of the NPC hit 
+                Debug.Log("Raycast Hitting NPC");
+
+                if (UI.activeInHierarchy) // might need work +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                {
+                    PickupScript.SetText(Hit.transform.gameObject.GetComponent<NPCDialogueScrpt>().NPCName);
+                }
+
+                if (Input.GetButtonDown("Pickup")) // E Key
+                {
+                    NPCInteractionScrpt.StartInteraction();
+                }
+            }
+
+            if (Hit.transform.tag == "Mirror")
+            {
+                Debug.Log("Raycast hitting mirror");
+
+                if (Input.GetButtonDown("Pickup")) // E Key
+                {
+                    MirrorTipScript.TipMirror();
+                }
+            }
+
+
+            else
+            {
+
+				if (Input.GetButtonDown("Pickup")) // E Key
+                {
+					AddHitToInv(Hit);
+				}
+
+				if (Hit.transform.gameObject.GetComponent<Item>())
+				{
+					PickupScript.SetText(Hit.transform.gameObject.GetComponent<Item>().ItemName.ToString());
+				}
+				else
+				{
+					if (UI.activeInHierarchy)
+					{
+						PickupScript.BlankText();
+					}
+				}
+            }
+		}
+	}
+
+
+
+	private void AddHitToInv(RaycastHit Hit)
+	{
+		if (Hit.transform.gameObject.GetComponent<Item>())
+		{
+			Hit.transform.gameObject.GetComponent<Item>().AddToInv(Hit.transform.gameObject.GetComponent<Item>());
+			Hit.transform.gameObject.transform.SetParent(GameObject.Find("InvItems").transform);
+			Hit.transform.gameObject.SetActive(false);
+			PickupScript.BlankText();
+		}
+	}
+
+
+	private void ToggleUI()
+	{
+		if (UIOpen)
+		{
+			UI.SetActive(true);
+		}
+
+		if (!UIOpen)
+		{
+			UI.SetActive(false);
+		}
+	}
+}
---
+++ UnityGameProject/Assets/Textures/Materials/Scripts/Samuel'sSuperStuff/Event_Start.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_Start : MonoBehaviour
+{
+    public bool Event;
+    //public GameObject Player;
+    public PlayerController MovementScript;
+    public Mouse_Move stopMouse;
+    public Enemy_Move AIMovment;
+    //public Entity AnimStop;
+    //public Event_Start Boolon;
+    Animator anim; //Assigns the animator
+    public FlyingPans PansMove;
+    public FlyingPans PansMove2;
+    public FlyingPans PansMove3;
+    public FlyingPans PansMove4;
+    public FlyingPans PansMove5;
+    public FlyingPans PansMove6;
+    public FlyingPans PansMove7;
+    public FlyingPans PansMove8;
+    public FlyingPans PansMove9;
+    public FlyingPans PansMove10;
+    public FlyingPans PansMove11;
+    public FlyingPans PansMove12;
+
+    
+
+    void Update()
+    {
+        if(Event == true)
+        {
+            AIMovment.enabled = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+
+
+
+
+
+            Debug.Log("running");
+            MovementScript.enabled = false;
+            stopMouse.enabled = false;
+            PansMove.enabled = true;
+            PansMove2.enabled = true;
+            PansMove3.enabled = true;
+            PansMove4.enabled = true;
+            PansMove5.enabled = true;
+            PansMove6.enabled = true;
+            PansMove7.enabled = true;
+            PansMove8.enabled = true;
+            PansMove9.enabled = true;
+            PansMove10.enabled = true;
+            PansMove11.enabled = true;
+            PansMove12.enabled = true;
+            Event = true;
+        }        
+    }
+}

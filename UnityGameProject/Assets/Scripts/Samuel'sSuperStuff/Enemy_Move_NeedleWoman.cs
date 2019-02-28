d78b74073afd01d6dad58aec4393772a8b6b0930
Edward Goodhew <ejgoodhew@btinternet.com>
2019-02-28 21:59:53 +0000

    Needle Woman now runs at player and stabs them no damage atm

---
+++ UnityGameProject/Assets/Scripts/Samuel'sSuperStuff/Enemy_Move_NeedleWoman.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_Move_NeedleWoman : MonoBehaviour
+{
+    public GameObject Player;   //sets object to follow
+
+    NavMeshAgent enemy; //Assigns the nav mesh
+
+    Animator anim; //Assigns the animator
+
+    public bool PlayerInRoom;
+
+    private int AmountOfStabs;
+
+   // public float RayLength;
+    private float TimeLeft = 3.0f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>(); //Calls animator
+        enemy = GetComponent<NavMeshAgent>(); //Calls nav mesh
+    }
+
+    void Update()
+    {
+            if ((Player) && (PlayerInRoom))
+            {
+                if (enemy.remainingDistance != Mathf.Infinity && enemy.remainingDistance >= enemy.stoppingDistance)
+                {
+                    enemy.destination = Player.transform.position;
+                    anim.SetBool("Running", true);
+                    anim.SetBool("Stab", false);
+                }
+
+                else if (enemy.remainingDistance != Mathf.Infinity && enemy.remainingDistance < enemy.stoppingDistance)
+                {
+                    if (AmountOfStabs == 2)
+                    {
+                        anim.SetBool("Running", false);
+                        anim.SetBool("Stab", false);
+                    }
+                    else
+                    {
+                        enemy.destination = Player.transform.position;
+                        anim.SetBool("Running", false);
+                        anim.SetBool("Stab", true);
+                    AmountOfStabs++;
+                    }
+                }
+            }
+            else
+            {
+                anim.SetBool("Running", false);
+                anim.SetBool("Stab", false);
+            Debug.Log("Go to idle");
+            }
+    }
+}

45a795bcac9e5017562dcce26243b80996e6c30f
Jonathan Carter <jmc@devj.uk>
2019-03-01 10:58:59 +0000

    Scene Changing Script

--- UnityGameProject/Assets/Scripts/JonathansScripts/EnterHouseScript.cs
+++ UnityGameProject/Assets/Scripts/JonathansScripts/EnterHouseScript.cs
@@ -3,11 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class EnterHouseScript : MonoBehaviour
 {
+	public enum Rooms
+	{
+		Outside,
+		Ground,
+		Upper,
+		Basement,
+		End,
+	}
+
+	public Rooms Scenes;
+
 	private void OnCollisionEnter(Collision collision)
 	{
-		if (collision.gameObject.tag == "Player")
+		switch (Scenes)
 		{
-			SceneManager.LoadSceneAsync("GroundFloorScene");
+			case Rooms.Outside:
+				break;
+			case Rooms.Ground:
+				if (collision.gameObject.tag == "Player")
+				{
+					SceneManager.LoadSceneAsync("GroundFloorScene");
+				}
+				break;
+			case Rooms.Upper:
+				if (collision.gameObject.tag == "Player")
+				{
+					SceneManager.LoadSceneAsync("UpperFloorScene");
+				}
+				break;
+			case Rooms.Basement:
+				break;
+			case Rooms.End:
+				break;
+			default:
+				break;
 		}
 	}
 }
